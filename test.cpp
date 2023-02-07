9b32a26078f5d0fd63b78152e34069c2f6f9fb09
Remi Etchebarne <retcheba@3C3.42angouleme.fr>
2023-02-07 17:54:48 +0100

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retcheba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 14:28:53 by retcheba          #+#    #+#             */
+/*   Updated: 2023/02/07 16:11:06 by retcheba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	char buff[500];
+
+	std::cout << "Hello World !" << std::endl;
+	
+	std::cout << "Input a message: ";
+	std::cin >> buff;
+	std::cout << "You entered: [" << buff << "]" << std::endl;
+
+	return (0);
+}
