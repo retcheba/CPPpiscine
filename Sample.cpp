f0e7d89890727aa649ff1f41971046b40804da20
Remi Etchebarne <retcheba@3C3.42angouleme.fr>
2023-02-08 18:44:02 +0100

    Class & Instance

---
+++ Sample.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sample.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retcheba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 18:36:21 by retcheba          #+#    #+#             */
+/*   Updated: 2023/02/08 18:39:47 by retcheba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Sample.hpp"
+
+Sample::Sample(void)
+{
+	std::cout << "Constructor called" << std::endl;
+	return;
+}
+
+Sample::~Sample(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
